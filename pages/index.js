fc0a86e73242b7a3dee90f144d37e34043f7d72d
Lucas Salgado Vieira <116003800+Lucas-S-Vieira@users.noreply.github.com>
2023-09-05 02:45:46 +0000

    adiciona pagina inicial

---
+++ pages/index.js
@@ -0,0 +1,10 @@
+function Home() {
+    return <><h1>Meu amor</h1>
+    <p>Eu te amo e tenho certeza que todos seus objetivos serão alcançados, numa das aulas do meu curso surgiu um desafio.</p>
+    <p>Criar uma pagina na internet com uma frase para alguem, primeira pessoa que veio em minha mente foi você.</p>
+    <p>Eu queria falar tantas coisas mas vou resumir a, Eu te amo Sara!</p>
+    <h2>Você é o amor da minha vida</h2>
+    </>
+}
+
+export default Home
