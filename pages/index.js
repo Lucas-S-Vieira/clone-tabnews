2cd4c980153b821fd5fd40c6066c32bda4eeb1e9
Lucas Salgado Vieira <116003800+Lucas-S-Vieira@users.noreply.github.com>
2023-10-05 13:09:25 +0000

    adiciona scripts `llint:check` e `lint:fix`

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,27 @@
 function Home() {
-    return <><h1>Meu amor</h1>
-    <p>Eu te amo e tenho certeza que todos seus objetivos serão alcançados, numa das aulas do meu curso surgiu um desafio.</p>
-    <p>Criar uma pagina na internet com uma frase para alguem, primeira pessoa que veio em minha mente foi você.</p>
-    <p>Eu queria falar tantas coisas mas vou resumir a, Eu te amo Sara!</p>
-    <h2>Você é o amor da minha vida!</h2>
+  return (
+    <>
+      <h1>Meu amor</h1>
+      <p>
+        Eu te amo e tenho certeza que todos seus objetivos serão alcançados,
+        numa das aulas do meu curso surgiu um desafio.
+      </p>
+      <p>
+        Criar uma pagina na internet com uma frase para alguem, primeira pessoa
+        que veio em minha mente foi você.
+      </p>
+      <p>Eu queria falar tantas coisas mas vou resumir a, Eu te amo Sara!</p>
+      <h2>Você é o amor da minha vida!</h2>
     </>
+  );
 }
 
-export default Home
+function teste() {
+  console.log("teste");
+}
+
+function teste2() {
+  console.log("teste 2");
+}
+
+export default Home;
